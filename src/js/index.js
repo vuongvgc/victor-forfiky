6cb7a1b63bcc0ef8354c5e0cf3607f0ac597746f
vuongvgc <vuongvgc@gmail.com>
2020-05-28 19:19:21 +0700

    update search modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,4 @@
-import axios from "axios";
-async function getResults(query) {
-  // const proxy = "https://cors-anywhere.herokuapp.com/";
-  // const key = "20031996";
-  try {
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?q=pizza`
-    );
-    const data = res.data;
-    console.log(data.recipes);
-  } catch (Error) {
-    console.log(Error);
-  }
-}
-getResults("pizza");
+import Search from "./models/search";
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
--- src/js/models/search.js
+++ src/js/models/search.js
@@ -1,1 +1,18 @@
-export default "I am export strings";
+import axios from "axios";
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/search?q=${this.query}`
+      );
+     // console.log(res);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
