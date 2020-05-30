4b2aa6980a78028852d29bba1b3915bdbcdb6f42
vuongvgc <vuongvgc@gmail.com>
2020-05-30 17:31:12 +0700

    I reSetup version of node to like version of course

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,36 @@
-import Search from "./models/search";
-const search = new Search("pizza");
-console.log(search);
-search.getResults();
+import Search from './models/Search';
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+/** 
+ * SEARCH CONTROLLER
+ */
+const controlSearch = async () => {
+    // 1. Get query from view
+    const query = 'pizza';
+    if (query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+        // 3. Prepare UI for results
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
+    
+            // 5. Render results on UI
+            console.log(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
+    }
+}
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
