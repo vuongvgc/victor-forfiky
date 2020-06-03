fbd5e995b3837f6a5d54d6fd18c1c4f1741ee0c3
vuongvgc <vuongvgc@gmail.com>
2020-06-03 20:40:59 +0700

    add function renderload use svg

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import Search from './models/Search';
-import {elements} from './views/base';
+import {elements, renderLoader} from './views/base';
 import * as searchView from './views/searchView';
 /** Global state of the app
  * - Search object
@@ -23,6 +23,7 @@ const controlSearch = async () => {
             // 3. Prepare UI for results
             searchView.clearInput();
             searchView.clearResults();
+            renderLoader(elements.searchLoader);
             // 4. Search for recipes
             await state.search.getResults();
     
@@ -33,7 +34,7 @@ const controlSearch = async () => {
             searchView.renderResults(state.search.result);
         } catch (err) {
             alert('Something wrong with the search...');
-            clearLoader();
+            console.log(err);
         }
     }
 }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -4,5 +4,17 @@ export const elements = {
     // 2. search input
     searchInput: document.querySelector('.search__field'),
     //3. Render UI
-    searchResList: document.querySelector('.results__list')
+    searchResList: document.querySelector('.results__list'),
+    //4. Renderreload
+    searchLoader: document.querySelector('.results')
+}
+export const renderLoader = parent => {
+    const loader = `
+        <div class ="loader">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin',loader);
 }
